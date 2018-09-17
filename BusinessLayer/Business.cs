ecdadf968d5e4c11b1aba3f59b5c8f4ef7622d31
Birhat Zorlu <zorlubirhat@gmail.com>
2018-09-17 16:51:51 +0300

    Add files via upload

---
+++ BusinessLayer/Business.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Business
+    {
+        WebServiceReference.ItemServiceClient client = new WebServiceReference.ItemServiceClient();
+
+        public List<object> returnFiles(string filename)
+        {
+            List<object> files = client.getFiles(filename);
+
+            return files;
+        }
+
+        public List<ItemCatalog.Items> returnItems(string file)
+        {
+            List<ItemCatalog.Items> items = client.getItems(file);
+
+            return items;
+        }
+
+        public ItemCatalog.Items returnSelectedItem(string file, int index)
+        {
+            ItemCatalog.Items item = returnItems(file)[index];
+
+            return item;
+        }
+
+        public ItemCatalog.Items createNewItem(string id, string author, string title, string genre, double price, string publishdate, string filename)
+        {
+            ItemCatalog.Items newitem = new ItemCatalog.Items(id, author, title, genre, price, publishdate);
+            ItemCatalog.ItemCatalog catalog = new ItemCatalog.ItemCatalog();
+
+            catalog = new ItemCatalog.ItemCatalog { items = client.getItems(filename) };
+            client.createNewItem(filename, catalog, newitem);
+
+            return newitem;
+        }
+
+        public ItemCatalog.ItemCatalog returnCatalog(string file)
+        {
+            ItemCatalog.ItemCatalog catalog = new ItemCatalog.ItemCatalog();
+            catalog = new ItemCatalog.ItemCatalog { items = client.getItems(file) };
+
+            return catalog;
+        }
+
+        public void editItem(string file, ItemCatalog.ItemCatalog catalog, ItemCatalog.Items item, int index)
+        {
+            client.editItem(file, catalog, item, index);
+        }
+    }
+}
---
+++ ItemCatalog/ItemCatalog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemCatalog
+{
+    [DataContract(Name = "catalog")]
+    public class ItemCatalog
+    {
+
+        [DataMember(EmitDefaultValue = false, IsRequired = true, Name = "books", Order = 1)]
+        public List<Items> items { get; set; }
+    }
+
+    [DataContract(Name = "book")]
+    public class Items
+    {
+        [DataMember(EmitDefaultValue = false, IsRequired = true, Name = "id", Order = 1)]
+        public string id { get; set; }
+        [DataMember(EmitDefaultValue = false, IsRequired = true, Name = "author", Order = 2)]
+        public string author { get; set; }
+        [DataMember(EmitDefaultValue = false, IsRequired = true, Name = "title", Order = 3)]
+        public string title { get; set; }
+        [DataMember(EmitDefaultValue = false, IsRequired = true, Name = "genre", Order = 4)]
+        public string genre { get; set; }
+        [DataMember(EmitDefaultValue = false, IsRequired = true, Name = "price", Order = 5)]
+        public double price { get; set; }
+        [DataMember(EmitDefaultValue = false, IsRequired = true, Name = "publish_date", Order = 6)]
+        public string publish_date { get; set; }
+
+        public Items(string bookid, string bookauthor, string booktitle, string bookgenre, double bookprice, string publishdate)
+        {
+            this.id = bookid;
+            this.author = bookauthor;
+            this.title = booktitle;
+            this.genre = bookgenre;
+            this.price = bookprice;
+            this.publish_date = publishdate;
+        }
+    }
+} 
